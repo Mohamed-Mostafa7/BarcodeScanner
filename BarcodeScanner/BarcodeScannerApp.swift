12ea994e711442d269eda46e80c149713d4bfefe
Mohamed-Mostafa7 <mohameds.mostafas7@gmail.com>
2024-09-14 12:47:56 +0300

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Mohamed Mostafa on 14/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
